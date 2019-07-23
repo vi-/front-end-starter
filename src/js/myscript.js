6b74c581f71f77418bd16f87aa731e03114ccf24
Victor Ivanov <reachvctr@gmail.com>
2019-07-23 05:59:29 +0000

    Requiring helpers.js

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -2,9 +2,10 @@ const objectFitImages = require("object-fit-images");
 
 objectFitImages();
 
-const nav = require("./modules/navigation.js");
+const nav 	= require("./modules/navigation.js");
+const h 	= require("./modules/helpers.js");
 
 const burger 	= document.querySelector('.hamburger'),
-			navEl 	= document.querySelector('.site-nav')
+	  navEl 	= document.querySelector('.site-nav');
 
 nav.init( burger, navEl );
